8878dbe0326066227b545318f1d93b7fc64b9d3a
Dirk Strauss <dirkstrauss@users.noreply.github.com>
2017-02-19 18:33:53 +0200

    Add files via upload

---
+++ VisualStudioTips/VisualStudioTips/SomeClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualStudioTips
+{
+    public class SomeClass
+    {
+        public string UsefulMethod()
+        {
+            return "somevalue";
+        }
+    }
+}
